e3861c60d1bfa01332e17b9f607e33e43adc1302
YeZhetai <38740209+YeZhetai@users.noreply.github.com>
2020-06-26 09:17:31 +1000

    实现了物品名称简易的模糊匹配

--- Aura/plugins/market/get_price.py
+++ Aura/plugins/market/get_price.py
@@ -1,6 +1,7 @@
 import yaml
 import json
 from urllib.request import Request, urlopen
+import re
 
 
 async def get_json(url):
@@ -13,14 +14,29 @@ async def get_json(url):
     return rq_data[0]
 
 
-async def get_price(item: str) -> str:
+async def match_item(user_input, item_list):
+    suggestions = []
+    pattern = '.*'.join(user_input)
+    regex = re.compile(pattern)
+    for item in item_list:
+        match = regex.search(item)
+        if match:
+            suggestions.append((len(match.group()), match.start(), item))
+    return [x for _, _, x in sorted(suggestions)]
+
+
+async def get_price(url: str) -> str:
     with open('DB.yaml', 'r', encoding='utf-8') as f:
         data = yaml.load(f.read(), Loader=yaml.Loader)
-    if item not in data:
-        return f'物品名称不正确，暂时仅支持完整物品名称'
+        item_list = []
+        for key in data:
+            item_list.append(key)
+    if url not in data:
+        return '查询到以下物品：\n.isk '+'\n.isk '.join(await match_item(url, item_list))
 
+    item = url
     item_id = data[item]
-    api_url = 'https://api.evemarketer.com/ec/marketstat/json?usesystem=30000142&typeid='+str(item_id)
+    api_url = 'https://api.evemarketer.com/ec/marketstat/json?usesystem=30000142&typeid=' + str(item_id)
     # evemarketer的API地址
     print(api_url)
     data = await get_json(api_url)
@@ -28,4 +44,3 @@ async def get_price(item: str) -> str:
     price = "{:,}".format(sell_data['min'])
 
     return f'欧服物价：\n{item}\n\n吉他最低售价 {price}\n\n价格来自EVEMARKETER'
-
