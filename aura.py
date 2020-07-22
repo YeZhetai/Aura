ba5c9f0afbe62151433be496abd4367f3ef99654
YeZhetai <38740209+YeZhetai@users.noreply.github.com>
2020-07-22 22:25:42 +1000

    删除了没有卵用的UI和打包文件

--- aura.py
+++ aura.py
@@ -5,9 +5,7 @@ import config
 from os import path
 
 
-def main_process():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+if __name__ == '__main__':
     nonebot.init(config)
     nonebot.load_plugins(
         path.join(path.dirname(path.abspath(__file__)), 'Aura', 'plugins'), 'Aura.plugins'
