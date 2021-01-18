50551f2f3e2b89980cacb9daa48f45ccf57fe5d3
Yezhetai <yezhetai@gmail.com>
2021-01-19 09:23:04 +1100

    更新了欧服市场信息到国服市场中心API

--- Aura/plugins/market/get_price.py
+++ Aura/plugins/market/get_price.py
@@ -1,5 +1,6 @@
 import yaml
 import json
+import requests
 from urllib.request import Request, urlopen
 import re
 
@@ -8,9 +9,12 @@ async def get_json(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) like Gecko'  # 伪装浏览器
     }
-    request = Request(url, headers=headers)
-    html = urlopen(request)
-    rq_data = json.loads(html.read())  # 这里取到的是一个包含字典的列表，我也不知道为啥，以后再说
+    request = requests.get(url, headers=headers)
+    if request.status_code == requests.codes.ok:
+        rq_data = request.json()
+    else:
+        rq_data = {request.status_code}
+    print(rq_data)
     return rq_data
 
 
@@ -25,7 +29,10 @@ async def match_item(user_input, item_list):
     return [x for _, _, x in sorted(suggestions)]
 
 
-async def data_process(data):
+async def data_process(url):
+    data = await get_json(url)
+    if data is list:
+        return data
     sell_data = data['sell']
     buy_data = data['buy']
     sell_min = "{:,.2f}".format(float(sell_data['min']))
@@ -46,17 +53,13 @@ async def get_price(url: str) -> str:
 
     item = url
     item_id = data[item]
-    of_api_url = 'https://market.fuzzwork.co.uk/aggregates/?system=30000142&types=' + str(item_id)
+    of_api_url = 'https://www.ceve-market.org/tqapi/market/region/10000002/system/30000142/type/'+str(item_id)+'.json'
     # evemarketer的API地址
     print(of_api_url)
+    of_price = await data_process(of_api_url)
     gf_api_url = 'https://www.ceve-market.org/api/market/region/10000002/system/30000142/type/'+str(item_id)+'.json'
     #
     print(gf_api_url)
-    of_api = await get_json(of_api_url)
-    gf_api = await get_json(gf_api_url)
-    of_data = of_api[str(item_id)]
-    gf_data = gf_api
-    of_price = await data_process(of_data)
-    gf_price = await data_process(gf_data)
+    gf_price = await data_process(gf_api_url)
 
     return f'{item}\n----------\n国服物价：\n{gf_price}\n----------\n欧服物价：\n{of_price}'
