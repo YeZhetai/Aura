44498fefbefede395928f7f8231de744b70ce2a3
Yezhetai <yezhetai@gmail.com>
2021-01-26 14:51:34 +1100

    更新到nonebot2

--- aura.py
+++ aura.py
@@ -1,13 +1,19 @@
-import asyncio
 
 import nonebot
-import config
+from nonebot.adapters.cqhttp import Bot as CQHTTPBot
 from os import path
 
 
+nonebot.init()
+
+nonebot.load_plugins("Aura/plugins")
+# 加载插件目录
+driver = nonebot.get_driver()
+driver.register_adapter("cqhttp", CQHTTPBot)
+nonebot.load_builtin_plugins()
+
+app = nonebot.get_asgi()
+
 if __name__ == '__main__':
-    nonebot.init(config)
-    nonebot.load_plugins(
-        path.join(path.dirname(path.abspath(__file__)), 'Aura', 'plugins'), 'Aura.plugins'
-    )
+
     nonebot.run()
