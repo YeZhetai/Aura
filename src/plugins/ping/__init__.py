86fb027d6a1731070fa235c3b2035e10e7a7a545
Ozhika <yezhetai@gmail.com>
2022-12-05 02:59:26 +1100

    update support for broadcasting

--- src/plugins/ping/__init__.py
+++ src/plugins/ping/__init__.py
@@ -1,13 +1,21 @@
+from nonebot import get_driver, get_bot
 from nonebot.matcher import Matcher
 from nonebot.adapters import Message
-from nonebot import get_bot
-from nonebot import on_command, on_message
+from nonebot import on_command
 from nonebot.rule import to_me
 from nonebot.params import Arg, CommandArg, ArgPlainText
 from nonebot.log import logger
+from nonebot_plugin_rauthman import isInService
+from nonebot.adapters.onebot.v11 import Event as onebot_event
+import datetime
+from .config import Config
 
 
-ping = on_command("ping", rule = to_me, priority = 5)
+plugin_config = Config.parse_obj(get_driver().config)
+
+
+ping = on_command("ping", rule = to_me(), priority = 5)
+broadcast = on_command("广播", rule = to_me() & isInService('broadcast', 10), priority = 5)
 
 
 @ping.handle()
@@ -29,17 +37,21 @@ async def receive(ping_data: Message = Arg(), ping_name: str = ArgPlainText("pin
     await bot.call_api(api="_send_group_notice",group_id=641726234, content = "1234")
 #    await ping.finish(ping_name)
 
-# @ping.got("check", prompt = "check")
-# async def check(check: Message = Arg(), check_cmd: str = ArgPlainText("check")):
-#     if check_cmd == "1":
-#         return True
 
+@broadcast.handle()
+async def _(matcher: Matcher, args: Message = CommandArg()):
+    stripped_arg = args.extract_plain_text()
 
-# Export something for other plugin
-# export = nonebot.export()
-# export.foo = "bar"
+    if stripped_arg:
+        matcher.set_arg("broadcast_data", args)
+    logger.info("broadcast is " + args)
 
-# @export.xxx
-# def some_function():
-#     pass
+@broadcast.got("broadcast_data", prompt="没有识别到内容，请发送广播的内容")
+async def receive(broadcast_data: Message = Arg(), broadcast_name: str = ArgPlainText("broadcast_data")):
+    logger.info("received broadcast is " + broadcast_name)
 
+    time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    message = '[CQ:at,qq=all]\n※※※  集结通知  ※※※\n{}\n{}'.format(time, broadcast_name) 
+    group_id = get_driver().config.broadcast_channel
+    bot = get_bot()
+    await bot.call_api(api="send_group_msg",group_id=group_id, message = message)
